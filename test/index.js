367ab83cd237ebf73f1a1400f3bd1f7e691804d8
João Neto <joaopintoneto@gmail.com>
2013-02-23 20:42:51 -0300

    Added tests

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ var MstubSchema = new mongoose.Schema({
 var MstubModel = mongoose.model('MstubModel', MstubSchema);
 
 describe('Suite', function() {
-  describe('## Global mongoose-mstub functions', function () {
+  describe('## Global mongoose-mstub functions tests', function () {
     afterEach(function (done) {
       // Unstub all after each test
       munstub(MstubModel);
@@ -79,7 +79,7 @@ describe('Suite', function() {
     });
   })
 
-  describe('## Module mongoose-mstub functions', function () {
+  describe('## Module mongoose-mstub functions tests', function () {
     afterEach(function (done) {
       // Unstub all after each test
       ms.munstub(MstubModel);
@@ -91,6 +91,57 @@ describe('Suite', function() {
       ms.mstub.should.be.a('function');
       should.exists(ms.munstub);
       ms.munstub.should.be.a('function');
+      done();
+    });
+
+    it('Should sutb method save overrided', function (done) {
+      var function_name;
+
+      function_name = MstubModel.prototype.save.prototype.constructor.name;
+      function_name.should.eql('save');
+
+      ms.mstub(MstubModel, 'save', function save_stub (err) {
+        callback(err);
+      });
+
+      function_name = MstubModel.prototype.save.prototype.constructor.name;
+      function_name.should.eql('save_stub');
+
+      done();
+    });
+
+    it('Should sutb method save overrided and method find still stub', function (done) {
+      var function_name;
+
+      // Stub save method
+      ms.mstub(MstubModel, 'save', function save_stub (err) {
+        callback(err);
+      });
+
+      // Stub find method
+      ms.mstub(MstubModel, 'find', function find_stub (err) {
+        callback(err);
+      });
+
+      // Assert method save is stub
+      function_name = MstubModel.prototype.save.prototype.constructor.name;
+      function_name.should.eql('save_stub');
+      
+      // Assert method find is stub
+      function_name = MstubModel.find.name;
+      function_name.should.eql('find_stub');
+
+      // Unstub only method save
+      ms.munstub(MstubModel, 'save');
+
+      // Assert method save is back
+      function_name = MstubModel.prototype.save.prototype.constructor.name;
+      function_name.should.eql('save');
+
+      // Assert metdhod find is stub
+      function_name = MstubModel.find.name;
+      function_name.should.eql('find_stub');
+
       done();
     });
   });
